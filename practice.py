3e799f185e2df50c5478ab56cc2e3df99381910d
Aunik Hussain <31704@longroad.ac.uk>
2014-12-14 23:10:20 +0000

    commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+
+
+
+def get_input():
+    while True:
+        stars = int(input("enter how many stars that is an odd integer"))
+        if stars % 2 and stars > 0:
+            break
+        else:
+            print("{0} is not a positive odd integer".format(stars))
+
+
+#main
+get_input()
